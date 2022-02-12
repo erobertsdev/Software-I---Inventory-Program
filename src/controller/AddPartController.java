0fccec243eebe40bc5455e756ebd2ae64825af73
Elias Roberts <elias@eroberts.dev>
2022-02-12 11:00:32 -0700

    removed static scene sizing

---
+++ src/controller/AddPartController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class AddPartController {
+
+    /** Declare variables for UI components **/
+
+    @FXML private RadioButton InHouseRadioButton;
+    @FXML private RadioButton OutsourcedRadioButton;
+    @FXML private TextField IDTextField;
+    @FXML private TextField NameTextField;
+    @FXML private TextField InvTextField;
+    @FXML private TextField PriceTextField;
+    @FXML private TextField MaxTextField;
+    @FXML private TextField MinTextField;
+    @FXML private TextField MachineIDTextField;
+    @FXML private Button SaveButton;
+    @FXML private Button CancelButton;
+
+    @FXML
+    public void handleAction() {
+        label.setText("OK Button pressed");
+    }
+}
