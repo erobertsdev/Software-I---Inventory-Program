df1b8a58bdb3a379b574c2eb43ec421888ccfa56
Elias Roberts <elias@eroberts.dev>
2022-03-01 03:35:28 -0700

    add and modify part/product buttons open new scenes

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -6,10 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TableView;
+import model.Part;
+import model.Product;
 
 import java.io.IOException;
 
@@ -17,12 +20,23 @@ public class MainController {
 
     /** Declare variables for UI components **/
 
-//    @FXML private TableView PartTable;
+    // TODO: LEFT OFF HERE
+    // Parts Pane
+    @FXML private TableView<Part> PartTable;
+    @FXML private TableColumn<Part, Integer> PartID;
+    @FXML private TableColumn<Part, String> PartName;
+    @FXML private TableColumn<Part, Integer> PartInvLevel;
+    @FXML private TableColumn<Part, Double> PartPrice;
     @FXML private Button AddPartButton;
     @FXML private Button ModifyPartButton;
     @FXML private Button DeletePartButton;
     @FXML private TextField PartSearch;
-//    @FXML private TableView ProductTable;
+    // Products Pane
+    @FXML private TableView<Product> ProductTable;
+    @FXML private TableColumn<Product, Integer> ProductID;
+    @FXML private TableColumn<Product, String> ProductName;
+    @FXML private TableColumn<Product, Integer> ProductInvLevel;
+    @FXML private TableColumn<Product, Double> ProductPrice;
     @FXML private Button AddProductButton;
     @FXML private Button ModifyProductButton;
     @FXML private Button DeleteProductButton;
@@ -40,7 +54,7 @@ public class MainController {
 
     public void handleModifyPartButton(ActionEvent event) throws IOException {
         // TODO: Add logic to figure out which part is selected
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("..\\view\\AddPartForm.fxml")));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("..\\view\\ModifyPartForm.fxml")));
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
